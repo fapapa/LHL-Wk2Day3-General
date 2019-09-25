86effdb49480a92d3199c8234b37f4efe1d59264
Fabio Papa <fabtheman@gmail.com>
2019-09-25 12:53:51 -0700

    Add client code to say hello from client
    Pair programmed with @user956

--- client.js
+++ client.js
@@ -6,3 +6,11 @@ const conn = net.createConnection({
 });
 
 conn.setEncoding('utf8'); // interpret data as text
+
+conn.on('data', (data) => {
+  console.log('Server says: ', data);
+});
+
+conn.on('connect', () => {
+  conn.write('Hello from client!');
+});
